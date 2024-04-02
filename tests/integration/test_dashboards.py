30c4643e1fc0a366383bcef49485efe23d8d8c1c
Serge Smertin <serge.smertin@databricks.com>
2024-04-02 17:47:07 +0200

    ..

--- tests/integration/test_dashboards.py
+++ tests/integration/test_dashboards.py
@@ -5,9 +5,8 @@ from databricks.sdk import WorkspaceClient
 from databricks.labs.lsql.dashboards import Dashboards
 
 
-def test_load_dashboard():
-    ws = WorkspaceClient(profile='logfood-master')
+def test_load_dashboard(ws):
     dashboards = Dashboards(ws)
-    src = "/Workspace/Users/serge.smertin@databricks.com/Databricks Labs GitHub telemetry.lvdash.json"
+    src = "/Workspace/Users/serge.smertin@databricks.com/Trivial Dashboard.lvdash.json"
     dst = Path(__file__).parent / "sample"
     dashboards.save_to_folder(src, dst)
