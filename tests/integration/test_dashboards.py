f424402987c1ddd7bb2407ac9ffda36dcf710af9
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-27 13:43:47 +0200

    Add test for creating counter specs

--- tests/integration/test_dashboards.py
+++ tests/integration/test_dashboards.py
@@ -3,6 +3,7 @@ from dataclasses import fields, is_dataclass
 from pathlib import Path
 
 from databricks.labs.lsql.dashboard import Dashboard
+from databricks.labs.lsql.lakeview.model import CounterSpec
 
 
 def test_load_dashboard(ws):
@@ -16,3 +17,16 @@ def test_dashboard_deploys_one_dataset_per_query(ws):
     queries = Path(__file__).parent / "queries"
     dashboard = Dashboard(ws).deploy(queries)
     assert len(dashboard.datasets) == len([query for query in queries.glob("*.sql")])
+
+
+def test_dashboard_deploys_one_counter_widget_per_query(ws):
+    queries = Path(__file__).parent / "queries"
+    dashboard = Dashboard(ws).deploy(queries)
+
+    counter_widgets = []
+    for page in dashboard.pages:
+        for layout in page.layout:
+            if isinstance(layout.widget.spec, CounterSpec):
+                counter_widgets.append(layout.widget)
+
+    assert len(counter_widgets) == len([query for query in queries.glob("*.sql")])
