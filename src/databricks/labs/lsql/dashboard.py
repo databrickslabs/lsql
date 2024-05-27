c38b8e4513f94d35b784ab29a931b6ca9d875664
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-27 14:00:06 +0200

    Deploy lakeview dashboard

--- src/databricks/labs/lsql/dashboard.py
+++ src/databricks/labs/lsql/dashboard.py
@@ -56,8 +56,7 @@ class Dashboard:  # TODO: Rename, maybe DashboardClient?
             yaml.safe_dump(page, f)
         assert True
 
-    @staticmethod
-    def deploy(dashboard_folder: Path) -> LakeviewDashboard:
+    def deploy(self, display_name: str, dashboard_folder: Path) -> LakeviewDashboard:
         """Deploy dashboard from code, i.e. configuration and queries."""
         datasets, layouts = [], []
         for query_path in dashboard_folder.glob("*.sql"):
@@ -75,8 +74,12 @@ class Dashboard:  # TODO: Rename, maybe DashboardClient?
             layouts.append(layout)
 
         page = Page(name=dashboard_folder.name, display_name=dashboard_folder.name, layout=layouts)
-        dashboard = LakeviewDashboard(datasets=datasets, pages=[page])
-        return dashboard
+        lakeview_dashboard = LakeviewDashboard(datasets=datasets, pages=[page])
+
+        dashboard = self._ws.lakeview.create(display_name, serialized_dashboard=json.dumps(lakeview_dashboard.as_dict()))
+        lakeview_dashboard.dashboard = dashboard  # TODO: Refactor to get rid of this assignment
+
+        return lakeview_dashboard
 
     def _format_sql_file(self, sql_query, query_path):
         with query_path.open("w") as f:
--- tests/integration/test_dashboards.py
+++ tests/integration/test_dashboards.py
@@ -12,14 +12,15 @@ def test_load_dashboard(ws):
 
 
 def test_dashboard_deploys_one_dataset_per_query(ws):
+def test_dashboard_deploys_one_dataset_per_query(ws, make_random):
     queries = Path(__file__).parent / "queries"
-    dashboard = Dashboard(ws).deploy(queries)
+    dashboard = Dashboard(ws).deploy(f"lsql-D{make_random()}", queries)
     assert len(dashboard.datasets) == len([query for query in queries.glob("*.sql")])
 
 
-def test_dashboard_deploys_one_counter_widget_per_query(ws):
+def test_dashboard_deploys_one_counter_widget_per_query(ws, make_random):
     queries = Path(__file__).parent / "queries"
-    dashboard = Dashboard(ws).deploy(queries)
+    dashboard = Dashboard(ws).deploy(f"lsql-D{make_random()}", queries)
 
     counter_widgets = []
     for page in dashboard.pages:
@@ -28,3 +29,14 @@ def test_dashboard_deploys_one_counter_widget_per_query(ws):
                 counter_widgets.append(layout.widget)
 
     assert len(counter_widgets) == len([query for query in queries.glob("*.sql")])
+
+
+def test_dashboard_deploys_dashboard(ws, make_random):
+    dashboard_display_name = f"lsql-D{make_random()}"
+    queries = Path(__file__).parent / "queries"
+    dashboard = Dashboard(ws).deploy(dashboard_display_name, queries)
+
+    dashboard = ws.lakeview.get(dashboard.dashboard.dashboard_id)
+
+    assert dashboard_display_name is not None
+    assert dashboard.display_name == dashboard_display_name
