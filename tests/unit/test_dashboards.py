97d80d2dbb25d6fbfd1246a8af34f69d96e1d115
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-28 15:07:07 +0200

    Fix test names

--- tests/unit/test_dashboards.py
+++ tests/unit/test_dashboards.py
@@ -8,14 +8,14 @@ from databricks.labs.lsql.dashboards import Dashboards
 from databricks.labs.lsql.lakeview.model import CounterSpec, Dashboard
 
 
-def test_dashboard_creates_one_dataset_per_query():
+def test_dashboards_creates_one_dataset_per_query():
     ws = create_autospec(WorkspaceClient)
     queries = Path(__file__).parent / "queries"
     dashboard = Dashboards(ws).create_dashboard(queries)
     assert len(dashboard.datasets) == len([query for query in queries.glob("*.sql")])
 
 
-def test_dashboard_creates_one_counter_widget_per_query():
+def test_dashboards_creates_one_counter_widget_per_query():
     ws = create_autospec(WorkspaceClient)
     queries = Path(__file__).parent / "queries"
     dashboard = Dashboards(ws).create_dashboard(queries)
@@ -29,7 +29,7 @@ def test_dashboard_creates_one_counter_widget_per_query():
     assert len(counter_widgets) == len([query for query in queries.glob("*.sql")])
 
 
-def test_dashboards_saves_sql_files_to_folder(tmp_path):
+def test_dashboardss_saves_sql_files_to_folder(tmp_path):
     ws = create_autospec(WorkspaceClient)
     queries = Path(__file__).parent / "queries"
     dashboard = Dashboards(ws).create_dashboard(queries)
@@ -38,9 +38,10 @@ def test_dashboards_saves_sql_files_to_folder(tmp_path):
     Dashboards(ws).save_to_folder(dashboard, destination)
 
     assert len(list(destination.glob("*.sql"))) == len(dashboard.datasets)
+    ws.assert_not_called()
 
 
-def test_dashboard_deploy_raises_value_error_with_missing_display_name_and_dashboard_id():
+def test_dashboards_deploy_raises_value_error_with_missing_display_name_and_dashboard_id():
     ws = create_autospec(WorkspaceClient)
     dashboards = Dashboards(ws)
     lakeview_dashboard = Dashboard([], [])
@@ -49,7 +50,7 @@ def test_dashboard_deploy_raises_value_error_with_missing_display_name_and_dashb
     ws.assert_not_called()
 
 
-def test_dashboard_deploy_raises_value_error_with_both_display_name_and_dashboard_id():
+def test_dashboards_deploy_raises_value_error_with_both_display_name_and_dashboard_id():
     ws = create_autospec(WorkspaceClient)
     dashboards = Dashboards(ws)
     lakeview_dashboard = Dashboard([], [])
@@ -58,7 +59,7 @@ def test_dashboard_deploy_raises_value_error_with_both_display_name_and_dashboar
     ws.assert_not_called()
 
 
-def test_dashboard_deploy_calls_create_with_display_name():
+def test_dashboards_deploy_calls_create_with_display_name():
     ws = create_autospec(WorkspaceClient)
     dashboards = Dashboards(ws)
     lakeview_dashboard = Dashboard([], [])
@@ -68,7 +69,7 @@ def test_dashboard_deploy_calls_create_with_display_name():
     ws.lakeview.update.assert_not_called()
 
 
-def test_dashboard_deploy_calls_update_with_dashboard_id():
+def test_dashboards_deploy_calls_update_with_dashboard_id():
     ws = create_autospec(WorkspaceClient)
     dashboards = Dashboards(ws)
     lakeview_dashboard = Dashboard([], [])
