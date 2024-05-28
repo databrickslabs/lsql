d283c82d67e442fc52772eed6fac3c8f13b9e099
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-28 12:51:47 +0200

    Call create when deploy with display name

--- tests/unit/test_dashboards.py
+++ tests/unit/test_dashboards.py
@@ -23,3 +23,11 @@ def test_dashboard_raises_value_error_with_both_display_name_and_dashboard_id():
     with pytest.raises(ValueError):
         dashboards.deploy_dashboard(lakeview_dashboard, display_name="test", dashboard_id="test")
     ws.assert_not_called()
+
+
+def test_dashboard_deploy_calls_create_with_display_name():
+    ws = create_autospec(WorkspaceClient)
+    dashboards = Dashboards(ws)
+    lakeview_dashboard = Dashboard([], [])
+    dashboards.deploy_dashboard(lakeview_dashboard, display_name="test")
+    ws.lakeview.create.assert_called_once()
