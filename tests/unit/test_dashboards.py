25d2b947a0be28e2fed6a1adf4b5482d315775f5
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-28 12:11:26 +0200

    Raise value error when stating display name and dashboard id

--- tests/unit/test_dashboards.py
+++ tests/unit/test_dashboards.py
@@ -14,3 +14,12 @@ def test_dashboard_raises_value_error_with_missing_display_name_and_dashboard_id
     with pytest.raises(ValueError):
         dashboards.deploy_dashboard(lakeview_dashboard)
     ws.assert_not_called()
+
+
+def test_dashboard_raises_value_error_with_both_display_name_and_dashboard_id():
+    ws = create_autospec(WorkspaceClient)
+    dashboards = Dashboards(ws)
+    lakeview_dashboard = Dashboard([], [])
+    with pytest.raises(ValueError):
+        dashboards.deploy_dashboard(lakeview_dashboard, display_name="test", dashboard_id="test")
+    ws.assert_not_called()
