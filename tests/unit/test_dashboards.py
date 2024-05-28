30d3959653ca3330d470ecb32ea2b389dc69008f
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-28 15:44:48 +0200

    Refactor lakeview_dashboard to dashboard

--- tests/unit/test_dashboards.py
+++ tests/unit/test_dashboards.py
@@ -65,26 +65,26 @@ def test_dashboards_creates_one_counter_widget_per_query():
 def test_dashboards_deploy_raises_value_error_with_missing_display_name_and_dashboard_id():
     ws = create_autospec(WorkspaceClient)
     dashboards = Dashboards(ws)
-    lakeview_dashboard = Dashboard([], [])
+    dashboard = Dashboard([], [])
     with pytest.raises(ValueError):
-        dashboards.deploy_dashboard(lakeview_dashboard)
+        dashboards.deploy_dashboard(dashboard)
     ws.assert_not_called()
 
 
 def test_dashboards_deploy_raises_value_error_with_both_display_name_and_dashboard_id():
     ws = create_autospec(WorkspaceClient)
     dashboards = Dashboards(ws)
-    lakeview_dashboard = Dashboard([], [])
+    dashboard = Dashboard([], [])
     with pytest.raises(ValueError):
-        dashboards.deploy_dashboard(lakeview_dashboard, display_name="test", dashboard_id="test")
+        dashboards.deploy_dashboard(dashboard, display_name="test", dashboard_id="test")
     ws.assert_not_called()
 
 
 def test_dashboards_deploy_calls_create_with_display_name():
     ws = create_autospec(WorkspaceClient)
     dashboards = Dashboards(ws)
-    lakeview_dashboard = Dashboard([], [])
-    dashboards.deploy_dashboard(lakeview_dashboard, display_name="test")
+    dashboard = Dashboard([], [])
+    dashboards.deploy_dashboard(dashboard, display_name="test")
 
     ws.lakeview.create.assert_called_once()
     ws.lakeview.update.assert_not_called()
@@ -93,8 +93,8 @@ def test_dashboards_deploy_calls_create_with_display_name():
 def test_dashboards_deploy_calls_update_with_dashboard_id():
     ws = create_autospec(WorkspaceClient)
     dashboards = Dashboards(ws)
-    lakeview_dashboard = Dashboard([], [])
-    dashboards.deploy_dashboard(lakeview_dashboard, dashboard_id="test")
+    dashboard = Dashboard([], [])
+    dashboards.deploy_dashboard(dashboard, dashboard_id="test")
 
     ws.lakeview.create.assert_not_called()
     ws.lakeview.update.assert_called_once()
