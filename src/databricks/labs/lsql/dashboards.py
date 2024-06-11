0a64dd8d68e67615843d53652ccab940aa067927
Cor <jczuurmond@protonmail.com>
2024-06-11 16:25:59 +0200

    Make constant all caps (#140)
    Resolves #139

--- src/databricks/labs/lsql/dashboards.py
+++ src/databricks/labs/lsql/dashboards.py
@@ -32,7 +32,7 @@ logger = logging.getLogger(__name__)
 
 
 class Dashboards:
-    _maximum_dashboard_width = 6
+    _MAXIMUM_DASHBOARD_WIDTH = 6
 
     def __init__(self, ws: WorkspaceClient):
         self._ws = ws
@@ -123,7 +123,7 @@ class Dashboards:
     def _get_position(self, spec: WidgetSpec, previous_position: Position) -> Position:
         width, height = self._get_width_and_height(spec)
         x = previous_position.x + previous_position.width
-        if x + width > self._maximum_dashboard_width:
+        if x + width > self._MAXIMUM_DASHBOARD_WIDTH:
             x = 0
             y = previous_position.y + previous_position.height
         else:
