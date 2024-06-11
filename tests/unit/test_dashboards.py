9034cf164bf7477efd33d4643959ad39af6f5282
Cor Zuurmond <jczuurmond@protonmail.com>
2024-06-11 12:28:06 +0200

    Test widget below text widget

--- tests/unit/test_dashboards.py
+++ tests/unit/test_dashboards.py
@@ -199,6 +199,20 @@ def test_dashboards_creates_dashboard_with_many_widgets_not_on_the_first_row(tmp
     ws.assert_not_called()
 
 
+def test_dashboards_creates_dashboard_with_widget_below_text_widget(tmp_path):
+    ws = create_autospec(WorkspaceClient)
+    with (tmp_path / f"000_counter.md").open("w") as f:
+        f.write("# Description")
+    with (tmp_path / f"010_counter.sql").open("w") as f:
+        f.write(f"SELECT 100 AS count")
+
+    lakeview_dashboard = Dashboards(ws).create_dashboard(tmp_path)
+    layout = lakeview_dashboard.pages[0].layout
+
+    assert layout[0].position.y < layout[1].position.y
+    ws.assert_not_called()
+
+
 @pytest.mark.parametrize("query_names", [["a", "b", "c"], ["01", "02", "10"]])
 def test_dashboards_creates_dashboards_with_widgets_sorted_alphanumerically(tmp_path, query_names):
     ws = create_autospec(WorkspaceClient)
