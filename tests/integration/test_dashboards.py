8050e579959b36aa9f1f766d83375cbacce186fd
Cor Zuurmond <jczuurmond@protonmail.com>
2024-06-12 10:40:16 +0200

    Add integration test for widget with config header

--- tests/integration/test_dashboards.py
+++ tests/integration/test_dashboards.py
@@ -123,15 +123,13 @@ def test_dashboard_deploys_dashboard_with_counter_variation(ws, make_dashboard, 
     assert ws.lakeview.get(sdk_dashboard.dashboard_id)
 
 
-def test_dashboard_deploys_dashboard_with_big_widget(ws, make_dashboard, tmp_path):
-    sdk_dashboard = make_dashboard()
-
+def test_dashboard_deploys_dashboard_with_big_widget(ws, dashboard_id, tmp_path):
     query = """-- --width 6 --height 3\nSELECT 82917019218921 AS big_number_needs_big_widget"""
-    with (tmp_path / "counter.sql").open("w") as f:
+    with (tmp_path / f"counter.sql").open("w") as f:
         f.write(query)
     dashboards = Dashboards(ws)
     lakeview_dashboard = dashboards.create_dashboard(tmp_path)
 
-    sdk_dashboard = dashboards.deploy_dashboard(lakeview_dashboard, dashboard_id=sdk_dashboard.dashboard_id)
+    sdk_dashboard = dashboards.deploy_dashboard(lakeview_dashboard, dashboard_id=dashboard_id)
 
     assert ws.lakeview.get(sdk_dashboard.dashboard_id)
