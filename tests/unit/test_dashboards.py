c11ff25a198ec143fa2bf91ebb2ba4a1535c3dab
Cor Zuurmond <jczuurmond@protonmail.com>
2024-06-13 11:01:26 +0200

    Add path to widget metadata

--- tests/unit/test_dashboards.py
+++ tests/unit/test_dashboards.py
@@ -1,4 +1,3 @@
-import dataclasses
 import logging
 from pathlib import Path
 from unittest.mock import create_autospec
@@ -42,7 +41,7 @@ def test_dashboard_configuration_from_and_as_dict_is_a_unit_function():
 
 
 def test_widget_metadata_replaces_width_and_height():
-    widget_metadata = WidgetMetadata("test", 1, 1, 1)
+    widget_metadata = WidgetMetadata(Path("test.sql"), 1, 1, 1)
     updated_metadata = widget_metadata.replace_from_arguments(["--width", "10", "--height", "10"])
     assert updated_metadata.width == 10
     assert updated_metadata.height == 10
@@ -50,14 +49,14 @@ def test_widget_metadata_replaces_width_and_height():
 
 @pytest.mark.parametrize("attribute", ["id", "order", "width", "height"])
 def test_widget_metadata_replaces_attribute(attribute: str):
-    widget_metadata = WidgetMetadata("test", 1, 1, 1)
+    widget_metadata = WidgetMetadata(Path("test.sql"), 1, 1, 1)
     updated_metadata = widget_metadata.replace_from_arguments([f"--{attribute}", "10"])
     assert str(getattr(updated_metadata, attribute)) == "10"
 
 
 def test_widget_metadata_as_dict():
     raw = {"id": "test", "order": 10, "width": 10, "height": 10}
-    widget_metadata = WidgetMetadata("test", 10, 10, 10)
+    widget_metadata = WidgetMetadata(Path("test.sql"), 10, 10, 10)
     assert widget_metadata.as_dict() == raw
 
 
@@ -337,6 +336,23 @@ def test_dashboards_creates_dashboards_with_widgets_order_overwrite(tmp_path):
     widget_names = [layout.widget.name for layout in lakeview_dashboard.pages[0].layout]
 
     assert "".join(widget_names) == "abecdf"
+    ws.assert_not_called()
+
+
+def test_dashboards_creates_dashboards_with_widget_ordered_using_id(tmp_path):
+    ws = create_autospec(WorkspaceClient)
+
+    for query_name in "bcdf":
+        with (tmp_path / f"{query_name}.sql").open("w") as f:
+            f.write("SELECT 1 AS count")
+
+    with (tmp_path / "z.sql").open("w") as f:
+        f.write("-- --id a\nSELECT 1 AS count")  # Should be first because id is 'a'
+
+    lakeview_dashboard = Dashboards(ws).create_dashboard(tmp_path)
+    widget_names = [layout.widget.name for layout in lakeview_dashboard.pages[0].layout]
+
+    assert "".join(widget_names) == "abcdef"
     ws.assert_not_called()
 
 
