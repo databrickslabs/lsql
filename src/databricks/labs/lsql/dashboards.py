f78efdcb54df577a48527da3b3ce47452105c6cf
Cor Zuurmond <jczuurmond@protonmail.com>
2024-06-17 14:33:27 +0200

    Move docs

--- src/databricks/labs/lsql/dashboards.py
+++ src/databricks/labs/lsql/dashboards.py
@@ -53,7 +53,14 @@ class DashboardMetadata:
 
 
 class BaseHandler:
-    """Base file handler."""
+    """Base file handler.
+
+    Handlers are based on a Python implementation for FrontMatter.
+
+    Sources:
+        https://frontmatter.codes/docs/markdown
+        https://github.com/eyeseast/python-frontmatter/blob/main/frontmatter/default_handlers.py
+    """
 
     def __init__(self, path: Path) -> None:
         self._path = path
@@ -79,7 +86,7 @@ class BaseHandler:
 
 
 class QueryHandler(BaseHandler):
-    """Handle query files.
+    """Handle query files."""
 
     Handlers are based on a Python implementation for FrontMatter.
 
