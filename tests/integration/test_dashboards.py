ffe815dad509ec9d097fa68db4f78351b439f303
Cor Zuurmond <jczuurmond@protonmail.com>
2024-06-27 10:37:08 +0200

    Show empty title

--- tests/integration/test_dashboards.py
+++ tests/integration/test_dashboards.py
@@ -280,10 +280,10 @@ def test_dashboards_deploys_dashboard_with_filters(ws, make_dashboard, tmp_path)
     assert ws.lakeview.get(sdk_dashboard.dashboard_id)
 
 
-def test_dashboard_deploys_dashboard_with_overrides_counter_as_table(ws, make_dashboard, tmp_path):
+def test_dashboard_deploys_dashboard_with_empty_title(ws, make_dashboard, tmp_path):
     sdk_dashboard = make_dashboard()
 
-    query = '-- --overrides \'{"spec": {"frame": {"showTitle": false}}}\'\nSELECT 102132 AS count'
+    query = '-- --overrides \'{"spec": {"frame": {"showTitle": true}}}\'\nSELECT 102132 AS count'
     (tmp_path / "counter.sql").write_text(query)
 
     dashboards = Dashboards(ws)
