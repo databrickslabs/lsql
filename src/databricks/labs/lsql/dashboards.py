7a0408cb39a070917a0bd5ce40ba9a64012ec463
Cor Zuurmond <jczuurmond@protonmail.com>
2024-07-09 14:49:34 +0200

    Check path directly

--- src/databricks/labs/lsql/dashboards.py
+++ src/databricks/labs/lsql/dashboards.py
@@ -617,7 +617,7 @@ class DashboardMetadata:
         """
         tile_ids = []
         for tile in self.tile_metadatas:
-            if not (tile.is_markdown() or tile.is_query()):
+            if tile.path is None:
                 raise ValueError(f"Tile path is required: {tile}")
             tile_ids.append(tile.id)
         counter = collections.Counter(tile_ids)
