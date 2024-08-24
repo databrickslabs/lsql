0a9fd91b8c9a61476eca2d86c6e40c4be9552492
Hari Selvarajan <105197202+HariGS-DB@users.noreply.github.com>
2024-08-24 08:59:21 +0100

    adding normalize-case option to databricks labs lsql fmt cmd (#254)
    databricks labs lsql fmt cmd be defaults normalizes the query to lower
    case. Some queries are case sensitive
    (ex UCX dashboard using map field keys)
    This PR adds a parameter normalize-case to the fmt cli cmd to allow user
    to not normalize the query text (default would be to normalize as
    before)
    
    Resolves #253

--- tests/unit/test_dashboards.py
+++ tests/unit/test_dashboards.py
@@ -832,6 +832,20 @@ def test_query_formats(query, query_formatted):
     assert QueryTile.format(query) == query_formatted
 
 
+def test_query_formats_no_normalize():
+    query = """ select a.request_params.clusterId, 
+    a.request_params.notebookId 
+    from system.access.audit AS a left outer join inventory.clusters AS c
+ON a.request_params.clusterId = c.cluster_id AND a.action_name = 'runCommand'"""
+    query_formatted = """SELECT
+  a.request_params.clusterId,
+  a.request_params.notebookId
+FROM system.access.audit AS a
+LEFT OUTER JOIN inventory.clusters AS c
+  ON a.request_params.clusterId = c.cluster_id AND a.action_name = 'runCommand'"""
+    assert QueryTile.format(query, False) == query_formatted
+
+
 @pytest.mark.parametrize(
     "query, query_transformed, database_to_replace",
     [
