d40cb30e5a417ff12627590d72c46b64045c637c
Cor Zuurmond <jczuurmond@protonmail.com>
2024-09-26 10:24:16 +0200

    Paramitrize test to test inventory catalog

--- tests/unit/test_deployment.py
+++ tests/unit/test_deployment.py
@@ -1,49 +1,58 @@
 from dataclasses import dataclass
 
+import pytest
+
 from databricks.labs.lsql.backends import MockBackend
 from databricks.labs.lsql.deployment import SchemaDeployer
 
 from . import views
 
 
-def test_deploys_schema() -> None:
+@pytest.mark.parametrize("inventory_catalog", ["hive_metastore", "inventory"])
+def test_deploys_schema(inventory_catalog: str) -> None:
     mock_backend = MockBackend()
     deployment = SchemaDeployer(
         sql_backend=mock_backend,
         inventory_schema="inventory",
         mod=views,
+        inventory_catalog=inventory_catalog,
     )
 
     deployment.deploy_schema()
 
-    assert mock_backend.queries == ["CREATE SCHEMA IF NOT EXISTS hive_metastore.inventory"]
+    assert mock_backend.queries == [f"CREATE SCHEMA IF NOT EXISTS {inventory_catalog}.inventory"]
 
 
-def test_deletes_schema() -> None:
+@pytest.mark.parametrize("inventory_catalog", ["hive_metastore", "inventory"])
+def test_deletes_schema(inventory_catalog: str) -> None:
     mock_backend = MockBackend()
     deployment = SchemaDeployer(
         sql_backend=mock_backend,
         inventory_schema="inventory",
         mod=views,
+        inventory_catalog=inventory_catalog,
     )
 
     deployment.delete_schema()
 
-    assert mock_backend.queries == ["DROP SCHEMA IF EXISTS hive_metastore.inventory CASCADE"]
+    assert mock_backend.queries == [f"DROP SCHEMA IF EXISTS {inventory_catalog}.inventory CASCADE"]
 
 
-def test_deploys_view() -> None:
+@pytest.mark.parametrize("inventory_catalog", ["hive_metastore", "inventory"])
+def test_deploys_view(inventory_catalog: str) -> None:
     mock_backend = MockBackend()
     deployment = SchemaDeployer(
         sql_backend=mock_backend,
         inventory_schema="inventory",
         mod=views,
+        inventory_catalog=inventory_catalog,
     )
 
     deployment.deploy_view("some", "some.sql")
 
     assert mock_backend.queries == [
-        "CREATE OR REPLACE VIEW hive_metastore.inventory.some AS SELECT\n  id,\n  name\nFROM hive_metastore.inventory.something"
+        f"CREATE OR REPLACE VIEW {inventory_catalog}.inventory.some AS SELECT\n  id,\n  name\n"
+        f"FROM {inventory_catalog}.inventory.something"
     ]
 
 
@@ -53,19 +62,21 @@ class Foo:
     second: bool
 
 
-def test_deploys_dataclass() -> None:
+@pytest.mark.parametrize("inventory_catalog", ["hive_metastore", "inventory"])
+def test_deploys_dataclass(inventory_catalog: str) -> None:
     mock_backend = MockBackend()
     deployment = SchemaDeployer(
         sql_backend=mock_backend,
         inventory_schema="inventory",
         mod=views,
+        inventory_catalog=inventory_catalog,
     )
     deployment.deploy_schema()
     deployment.deploy_table("foo", Foo)
     deployment.delete_schema()
 
     assert mock_backend.queries == [
-        "CREATE SCHEMA IF NOT EXISTS hive_metastore.inventory",
-        "CREATE TABLE IF NOT EXISTS hive_metastore.inventory.foo (first STRING NOT NULL, second BOOLEAN NOT NULL) USING DELTA",
-        "DROP SCHEMA IF EXISTS hive_metastore.inventory CASCADE",
+        f"CREATE SCHEMA IF NOT EXISTS {inventory_catalog}.inventory",
+        f"CREATE TABLE IF NOT EXISTS {inventory_catalog}.inventory.foo (first STRING NOT NULL, second BOOLEAN NOT NULL) USING DELTA",
+        f"DROP SCHEMA IF EXISTS {inventory_catalog}.inventory CASCADE",
     ]
