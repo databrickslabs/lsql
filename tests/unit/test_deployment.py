d50d5ba13396eed724f40f64826fcc65cb4988c6
Cor Zuurmond <jczuurmond@protonmail.com>
2024-09-26 10:28:16 +0200

    Order tests like methods

--- tests/unit/test_deployment.py
+++ tests/unit/test_deployment.py
@@ -43,26 +43,6 @@ def test_deletes_schema(caplog, inventory_catalog: str) -> None:
     assert f"Deleting {inventory_catalog}.inventory database" in caplog.messages
 
 
-@pytest.mark.parametrize("inventory_catalog", ["hive_metastore", "inventory"])
-def test_deploys_view(caplog, inventory_catalog: str) -> None:
-    mock_backend = MockBackend()
-    deployment = SchemaDeployer(
-        sql_backend=mock_backend,
-        inventory_schema="inventory",
-        mod=views,
-        inventory_catalog=inventory_catalog,
-    )
-
-    with caplog.at_level(logging.INFO, logger="databricks.labs.lsql.deployment"):
-        deployment.deploy_view("some", "some.sql")
-
-    assert mock_backend.queries == [
-        f"CREATE OR REPLACE VIEW {inventory_catalog}.inventory.some AS SELECT\n  id,\n  name\n"
-        f"FROM {inventory_catalog}.inventory.something"
-    ]
-    assert f"Ensuring {inventory_catalog}.inventory.some view matches some.sql contents" in caplog.messages
-
-
 @dataclass
 class Foo:
     first: str
@@ -86,3 +66,23 @@ def test_deploys_dataclass(caplog, inventory_catalog: str) -> None:
         f"CREATE TABLE IF NOT EXISTS {inventory_catalog}.inventory.foo (first STRING NOT NULL, second BOOLEAN NOT NULL) USING DELTA",
     ]
     assert f"Ensuring {inventory_catalog}.inventory.foo table exists" in caplog.messages
+
+
+@pytest.mark.parametrize("inventory_catalog", ["hive_metastore", "inventory"])
+def test_deploys_view(caplog, inventory_catalog: str) -> None:
+    mock_backend = MockBackend()
+    deployment = SchemaDeployer(
+        sql_backend=mock_backend,
+        inventory_schema="inventory",
+        mod=views,
+        inventory_catalog=inventory_catalog,
+    )
+
+    with caplog.at_level(logging.INFO, logger="databricks.labs.lsql.deployment"):
+        deployment.deploy_view("some", "some.sql")
+
+    assert mock_backend.queries == [
+        f"CREATE OR REPLACE VIEW {inventory_catalog}.inventory.some AS SELECT\n  id,\n  name\n"
+        f"FROM {inventory_catalog}.inventory.something"
+    ]
+    assert f"Ensuring {inventory_catalog}.inventory.some view matches some.sql contents" in caplog.messages
